b90ac8f398c8c679b5581d05c84935bf16c299fb
ardririy <hynnkgw27@gmail.com>
2024-09-22 17:19:20 +0900

    特定のアイテムで耕地に栄養を与えられるように変更

--- src/main/java/com/github/ardririy/mine_farming/EventListener.java
+++ src/main/java/com/github/ardririy/mine_farming/EventListener.java
@@ -32,19 +32,49 @@ public class EventListener implements Listener {
                 }
             } else if(clickedBlock != null && (clickedBlock.getType() == Material.FARMLAND)){
                 // 耕地に右クリックした場合
-                if (item != null && (item.getType() == Material.BOOK)) {
-                    // 本を持っているなら耕地の情報を表示
-                    Position pos = new Position(clickedBlock.getX(), clickedBlock.getY(), clickedBlock.getZ());
-                    Farmland farmland = farmlandData.get(pos);
-                    Player player = event.getPlayer();
-                    if(farmland == null) {
-                        player.sendMessage("耕地の情報が見つかりません(Maybe bug?)");
-                    } else {
-                        player.sendMessage("耕地 " + pos + " :");
-                        player.sendMessage("  水分量: " + farmland.moisture);
-                        player.sendMessage("  窒素: " + farmland.nitrogen);
-                        player.sendMessage("  リン: " + farmland.phosphorus);
-                        player.sendMessage("  カリウム: " + farmland.kalium);
+                Position pos = new Position(clickedBlock.getX(), clickedBlock.getY(), clickedBlock.getZ());
+                Farmland farmland = farmlandData.get(pos);
+                Player player = event.getPlayer();
+                if (farmland == null) {
+                    player.sendMessage("耕地の情報が見つかりません(Maybe bug?)");
+                    return;
+                }
+
+                if(item != null) {
+                    switch (item.getType()) {
+                        case Material.BOOK -> {
+                            // 本を持っているなら耕地の情報を表示
+                            player.sendMessage("耕地 " + pos + " :");
+                            player.sendMessage("  水分量: " + farmland.moisture);
+                            player.sendMessage("  窒素: " + farmland.nitrogen);
+                            player.sendMessage("  リン: " + farmland.phosphorus);
+                            player.sendMessage("  カリウム: " + farmland.kalium);
+                        }
+                        case Material.POTION -> {
+                            // (水入りに限らないが)瓶: 水 +20
+                            event.setCancelled(true); // 水を飲むモーションを止める
+                            farmland.moisture += 20;
+                        }
+                        case Material.ROTTEN_FLESH -> {
+                            // 腐った肉: (5, 23, 0, 6)
+                            farmland.moisture += 5;
+                            farmland.nitrogen += 23;
+                            farmland.kalium += 6;
+                        }
+                        case Material.SUGAR_CANE -> {
+                            // サトウキビ: (3, 2, 21, 4)
+                            farmland.moisture += 3;
+                            farmland.nitrogen += 5;
+                            farmland.phosphorus += 21;
+                            farmland.kalium += 4;
+                        }
+                        case Material.BONE_MEAL -> {
+                            // 骨: (-8, 1, 0, 21)
+                            farmland.moisture = Math.max(farmland.moisture - 8, 0);
+                            farmland.nitrogen += 1;
+                            farmland.kalium += 21;
+                        }
+                        default -> { }
                     }
                 }
             }
